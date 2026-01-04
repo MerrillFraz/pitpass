72556fb3f2df276834fb2a562d021df9bf0b9b78
Merrill Frazier <merrill.frazier@gmail.com>
2026-01-04 16:17:16 -0500

    fix(backend): allow cors requests from vite dev server

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:8080'
+  origin: 'http://localhost:5173'
 }));
 
 app.get('/', (req: Request, res: Response) => {
